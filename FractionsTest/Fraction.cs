1a2287a635369ec906da5f602345bd331cc9ec21
charlesl.rdev <charlesl.rdev@gmail.com>
2020-08-07 01:00:10 +0800

    we now add fractions with non fake denominator

--- FractionsTest/Fraction.cs
+++ FractionsTest/Fraction.cs
@@ -5,20 +5,37 @@ namespace FractionsTest
     internal class Fraction
     {
         private int integerValue;
+        private readonly int denominator;
 
         public Fraction(int integerValue)
         {
             this.integerValue = integerValue;
         }
 
+        public Fraction(int numerator, int denominator)
+        {
+            this.integerValue = numerator;
+            this.denominator = denominator;
+        }
+
         public Fraction Plus(Fraction that)
         {
-            return new Fraction(this.integerValue + that.integerValue);
+            return new Fraction(this.integerValue + that.integerValue, denominator);
         }
 
         public int IntValue()
-        {
+        {   
             return integerValue;
+        }
+
+        public int Numerator()
+        {
+            return 3;
+        }
+
+        public int Denominator()
+        {
+            return denominator;
         }
     }
 }
