48081c51593271037af52e4e8ee417651b4fc8a8
charlesl.rdev <charlesl.rdev@gmail.com>
2020-08-13 16:58:32 +0800

    remove obsolete code and added some notes

--- FractionsTest/Fraction.cs
+++ FractionsTest/Fraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace FractionsTest
 {
@@ -22,23 +23,10 @@ namespace FractionsTest
             return new Fraction(numerator + that.numerator , denominator);
         }
 
-        public int IntValue()
-        {   
-            return numerator;
-        }
-
-        public int GetNumerator()
-        {
-            return numerator;
-        }
-
-        public int GetDenominator()
-        {
-            return denominator;
-        }
-
         public override bool Equals(object other)
-        {
+        {   
+            // default implementation of equals is based on identity and uses GetHashCode()
+            // based in hashCode or memory location.
             if(other is Fraction)
             {
                 var that = other as Fraction;
@@ -46,6 +34,15 @@ namespace FractionsTest
             }
 
             return false;
+        }
+
+        public override int GetHashCode()
+        {            
+            // hash code is important for hash map search instead of linear search.
+            // dive deeper into this. What is hashcode does in C#.
+            // return 0 - legal implementation of hashcode.
+            // if you return a constant for get hash code, it works but it would be converted to linear search.
+            return numerator * 19 + denominator;
         }
     }
 }
