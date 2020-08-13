d6cc193113cdae68557e72bfb057f1c400c4e54a
charlesl.rdev <charlesl.rdev@gmail.com>
2020-08-13 16:34:41 +0800

    intellicode magic

--- FractionsTest/AddFractionsTests.cs
+++ FractionsTest/AddFractionsTests.cs
@@ -10,35 +10,35 @@ namespace FractionsTest
         public void When_adding_zeroes()
         {
             var sum = new Fraction(0).Plus(new Fraction(0));
-            Assert.That(sum.IntValue(), Is.EqualTo(0));
+            Assert.That(sum, Is.EqualTo(new Fraction(0)));
         }
 
         [Test]
         public void When_adding_NonZero_plus_Zero()
         {
             var sum = new Fraction(3).Plus(new Fraction(0));
-            Assert.That(sum.IntValue(), Is.EqualTo(3));
+            Assert.That(sum, Is.EqualTo(new Fraction(3)));
         } 
         
         [Test]
         public void When_adding_Zero_plus_NonZero()
         {
             var sum = new Fraction(0).Plus(new Fraction(3));
-            Assert.That(sum.IntValue(), Is.EqualTo(3));
+            Assert.That(sum, Is.EqualTo(new Fraction(3)));
         }   
 
         [Test]
         public void When_adding_NonNegative_NonZero()
         {
             var sum = new Fraction(4).Plus(new Fraction(3));
-            Assert.That(sum.IntValue(), Is.EqualTo(7));
+            Assert.That(sum, Is.EqualTo(new Fraction(7)));
         }
         
         [Test]
         public void When_adding_Negative_NonNegative()
         {
             var sum = new Fraction(-3).Plus(new Fraction(1));
-            Assert.That(sum.IntValue(), Is.EqualTo(-2));
+            Assert.That(sum, Is.EqualTo(new Fraction(-2)));
         }
 
         // 1/5 + 2/5 = 3/5
